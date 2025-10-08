3d5b391fca78415aa890c43f30fba2a3c281577a
jonloucks <j.loucks@comcast.net>
2025-10-08 04:01:42 -0400

    Added Repository.keep for use case where promisor has same life as Repository (#45)
    * Removed unused step in release workflow
    
    * module info review and Repository.keep

--- contracts-test/src/test/java/module-info.java
+++ contracts-test/src/test/java/module-info.java
@@ -1,8 +1,8 @@
+/**
+ * Module to run tests on test tools
+ */
 module io.github.jonloucks.contracts.test.run {
-    requires io.github.jonloucks.contracts.test;
-    requires org.junit.jupiter.api;
-    requires org.mockito.junit.jupiter;
-    
+    requires transitive io.github.jonloucks.contracts.test;
+
     exports io.github.jonloucks.contracts.test.run to org.junit.platform.commons;
-    
 }
