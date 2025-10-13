ab6854cdcaa35ff28a5afa8830e5a2c2b8213894
Jon Loucks <j.loucks@comcast.net>
2025-10-13 10:25:25 -0400

    AutoOpen.NONE and AutoClose.NONE tests

--- contracts-test/src/main/java/io/github/jonloucks/contracts/test/Tests.java
+++ contracts-test/src/main/java/io/github/jonloucks/contracts/test/Tests.java
@@ -4,6 +4,7 @@ package io.github.jonloucks.contracts.test;
  * All the tests for a Contracts implementation as well as tests for testing tools.
  */
 public interface Tests extends
+    AutoTests,
     TestTests,
     ContractTests,
     ContractsTests,
